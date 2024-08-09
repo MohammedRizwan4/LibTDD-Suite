71d69a5afedf6e4bc86161d28c69125b67eb52db
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-09 19:53:30 +0530

    Passed TestCase: Blank Library name validation added

--- src/main/java/com/rizwan/Library.java
+++ src/main/java/com/rizwan/Library.java
@@ -5,7 +5,7 @@ public class Library {
     String name;
 
     public Library(String name) {
-        if(name == null) {
+        if(name == null || name.isBlank()) {
             throw new IllegalArgumentException("Library Name Should not be null");
         }
         this.name = name;
