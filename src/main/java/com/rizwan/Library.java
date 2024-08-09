49639ca24d36872dbdd1763a649505810fb17580
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-09 19:58:42 +0530

    Passed TestCase: Library name should be greater than 4 characters

--- src/main/java/com/rizwan/Library.java
+++ src/main/java/com/rizwan/Library.java
@@ -5,8 +5,8 @@ public class Library {
     String name;
 
     public Library(String name) {
-        if(name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Library Name Should not be null");
+        if(name == null || name.isBlank() || name.length() <= 4) {
+            throw new IllegalArgumentException("Library Name Should not be null or empty or should have atleast 4 characters");
         }
         this.name = name;
     }
