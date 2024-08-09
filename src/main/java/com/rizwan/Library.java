9f062c4a8111febc4a2e40150f6b7d89155a4017
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-09 20:30:33 +0530

    [Refactor]: AddBook method added to Book Class

--- src/main/java/com/rizwan/Library.java
+++ src/main/java/com/rizwan/Library.java
@@ -1,13 +1,20 @@
 package com.rizwan;
 
+import java.util.Map;
+
 public class Library {
 
     String name;
+    private Map<String, Book> bookInventory;
 
     public Library(String name) {
         if(name == null || name.isBlank() || name.length() <= 4) {
             throw new IllegalArgumentException("Library Name Should not be null or empty or should have atleast 4 characters");
         }
         this.name = name;
+    }
+
+    public void addBook(Book book) {
+        bookInventory.put(book.getISBN(), book);
     }
 }
