6233b386f3b539e9f8da1f4b0983a8f192340371
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-10 20:26:06 +0530

    Failed TestCase: failing a test case borrowing a book that does not exist.

--- src/test/java/com/rizwan/LibraryTest.java
+++ src/test/java/com/rizwan/LibraryTest.java
@@ -126,4 +126,16 @@ class LibraryTest {
         Book borrowedBook = library.getBookByISBN("9780132350884");
         assertNull(borrowedBook, "borrowedBook should be null as it has been borrowed earlier.");
     }
+
+    @Test
+    public void testShouldThrowExceptionWhenBookNotFoundDuringBorrowRequest() {
+
+        User user = new User("rizwan", Role.USER);
+
+        library.addUser(user);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> library.borrowBook(user, "9780132350884"));
+        assertEquals("Book not found", exception.getMessage());
+    }
+
 }
