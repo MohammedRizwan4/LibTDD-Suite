4cc844a6e1fbd633ddd72c28f0a52f58a97821a7
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-10 21:15:36 +0530

    Failed TestCase: failing a test case borrowing a book that is already borrowed

--- src/test/java/com/rizwan/LibraryTest.java
+++ src/test/java/com/rizwan/LibraryTest.java
@@ -138,4 +138,24 @@ class LibraryTest {
         assertEquals("Book not found", exception.getMessage());
     }
 
+    @Test
+    public void testShouldThrowExceptionWhenBookIsAlreadyBorrowed() {
+
+        User librarian = new User("Rizwan", Role.LIBRARIAN);
+        User user1 = new User("raiyan", Role.USER);
+        User user2 = new User("usman", Role.USER);
+        Book book = new Book("9780132350884", "Clean Code", "Robert Cecil Martin", Year.of(2012));
+
+        library.addUser(librarian);
+        library.addUser(user1);
+        library.addUser(user2);
+        library.addBook(librarian, book);
+
+        library.borrowBook(user1, "9780132350884");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> library.borrowBook(user2, "9780132350884"));
+        assertEquals("Book is already borrowed", exception.getMessage());
+    }
+
+
 }
